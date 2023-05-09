b188640dce42c5adb8ef03e318901f9743487c32
Ritesh <ritesh.comforttechno@gmail.com>
2023-05-09 22:48:48 +0530

    product section completed

--- src/components/ProudProducts.jsx
+++ src/components/ProudProducts.jsx
@@ -1,3 +1,4 @@
+import { Link } from "react-router-dom";
 import { products } from "../assets/data/ProductsData";
 
 export default function ProudProducts() {
@@ -6,7 +7,8 @@ export default function ProudProducts() {
       <h2 className="text-2xl font-bold mb-8">Products we are proud of</h2>
       <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-5">
         {products.slice(0, 8).map((product) => (
-          <div
+          <Link
+            to={`product/${product.id}`}
             key={product.id}
             className="border-2 border-gray-300 flex flex-col items-center justify-center hover:border-gray-800 transition-all duration-300"
           >
@@ -21,7 +23,7 @@ export default function ProudProducts() {
               <p className="text-lg font-normal">{product.description}</p>
               <span className="text-lg font-bold">${product.price}</span>
             </div>
-          </div>
+          </Link>
         ))}
       </div>
     </section>
