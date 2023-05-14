1282cbbd5877bad30e6c37c123b6f1ac6d8649b3
Ritesh <ritesh.comforttechno@gmail.com>
2023-05-14 16:03:01 +0530

    text center

--- src/pages/NotFound.jsx
+++ src/pages/NotFound.jsx
@@ -4,7 +4,7 @@ export default function NotFound() {
   return (
     <section className="container mx-auto px-5">
       <div className="flex h-screen flex-col gap-10 items-center justify-center">
-        <h2 className="text-3xl font-semibold uppercase text-blue-900">
+        <h2 className="text-3xl font-semibold uppercase text-blue-900 text-center">
           This Page is currently not Available
         </h2>
         <Link
